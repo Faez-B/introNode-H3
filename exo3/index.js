2eae766541196f5c1255e8b37d7a8eb48f5b92b4
Faez-B <faezbhatti@gmail.com>
2022-04-28 16:03:25 +0200

    Affichage du contenu dans le fichier index.html depuis index.js

---
+++ exo3/index.js
@@ -0,0 +1,7 @@
+// console.log(__dirname + "/assets/index.html");
+// console.log(__filename);
+
+const { readFileSync } = require('fs');
+
+// console.log(readFileSync(__dirname + "/assets/index.html"));
+console.log(readFileSync(__dirname + "/assets/index.html", "utf8"));
